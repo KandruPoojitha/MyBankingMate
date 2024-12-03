f22f399ac34789365a3dfe76b80d024a16055961
KandruPoojitha <poojithakandru.ed@gmail.com>
2024-12-03 00:13:05 -0500

    modified home activity and added nav bar

--- app/src/main/java/com/example/mybankmate/AdminActivity.java
+++ app/src/main/java/com/example/mybankmate/AdminActivity.java
@@ -24,7 +24,6 @@ public class AdminActivity extends AppCompatActivity {
         adminManagementButton = findViewById(R.id.adminManagementButton);
         logoutButton = findViewById(R.id.logoutButton);
 
-        // Navigate to User Management
         userManagementButton.setOnClickListener(v -> {
             Intent intent = new Intent(AdminActivity.this, UserManagementActivity.class);
             startActivity(intent);
